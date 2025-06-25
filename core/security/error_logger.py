c5e265c69cc7567c1827a1d2efb54f8575cc39cf
Tsoympet <tsoympet7@gmail.com>
2025-06-25 22:14:40 +0300

    Update error_logger.py

--- core/security/error_logger.py
+++ core/security/error_logger.py
@@ -1,7 +1,11 @@
 import logging
+import os
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
 
 logger = logging.getLogger("GAIA_ErrorLogger")
-handler = logging.FileHandler("logs/errors.log")
+handler = logging.FileHandler(os.path.join(LOG_DIR, "errors.log"))
 formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
